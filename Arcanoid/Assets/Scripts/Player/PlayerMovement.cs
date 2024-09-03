8b30f94ad8cd4ecd9ce2cff99a45bfaa8df1fb2d
Petrov Dmitry <petrovdmtry@gmail.com>
2024-09-03 23:16:16 +0300

    WIP. player movement realization
    Player stop when bounds arrived

--- Arcanoid/Assets/Scripts/Player/PlayerMovement.cs
+++ Arcanoid/Assets/Scripts/Player/PlayerMovement.cs
@@ -10,11 +10,21 @@ namespace SekiburaGames.Arkanoid.Gameplay
     {
         [SerializeField]
         private GameObject _playerGO;
+
+        [SerializeField]
+        private Renderer _playerRender;
+        [SerializeField]
+        private Renderer _leftBound;
+        [SerializeField]
+        private Renderer _rightBound;
+
         private InputController _inputController;
         private float _inputValue;
+
         private void Start()
         {
             SystemManager.Get(out _inputController);
+            
             _inputController.PlayerMovePerformed += (value) => _inputValue = value;
             _inputController.PlayerMoveCanceled += () => _inputValue = 0;
         }
@@ -23,8 +33,10 @@ namespace SekiburaGames.Arkanoid.Gameplay
         {
             if (GameStatesManager.gameState != StaticData.AvailableGameStates.Playing)
                 return;
-
-            //Debug.Log($"ProceedInput {_inputValue}");
+            if (((_playerGO.transform.position.x + _playerRender.bounds.size.x/2) >= (_rightBound.transform.position.x - _rightBound.bounds.size.x/2)) && _inputValue > 0)
+                return;
+            else if (((_playerGO.transform.position.x - _playerRender.bounds.size.x / 2) <= (_leftBound.transform.position.x + _leftBound.bounds.size.x / 2)) && _inputValue < 0)
+                return;
 
             if (Mathf.Abs(_inputValue) > 0.1f)
             {
