74ca213d42d818a410bd609d3fec3403b7bdbd2f
Petrov Dmitry <petrovdmtry@gmail.com>
2024-09-03 11:04:33 +0300

    added base ui system

---
+++ Arcanoid/Assets/Scripts/UI/Base/UI_ButtonOpenerView.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SekiburaGames.Arcanoid.UI
+{
+    public class UI_ButtonOpenerView : MonoBehaviour
+    {
+        [SerializeField]
+        private Button _button;
+
+        [SerializeField]
+        private View _view;
+
+        [SerializeField]
+        private bool _remember = true;
+        [SerializeField]
+        private bool _hideLast = true;
+
+        private void Start()
+        {
+            _button.onClick.AddListener(() =>
+            {
+                ViewManager.Show(_view, _remember, _hideLast);
+            });
+        }
+    }
+}
