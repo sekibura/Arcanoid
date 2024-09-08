4d22ab270c9578f3039952f6609bb6f870e89ec9
Petrov Dmitry <petrovdmtry@gmail.com>
2024-09-08 22:07:40 +0300

    Pause & setting menu

--- Arcanoid/Assets/Scripts/UI/Views/GameplayView.cs
+++ Arcanoid/Assets/Scripts/UI/Views/GameplayView.cs
@@ -1,11 +1,29 @@
+using SekiburaGames.Arkanoid.System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace SekiburaGames.Arkanoid.UI
 {
     public class GameplayView : View
     {
+        [SerializeField]
+        private Button _pauseBtn;
+        private GameStateMachine _gameStateMachine;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            SystemManager.Get(out _gameStateMachine);
+            _pauseBtn.onClick.AddListener(() => OnPauseButtonPressed());
+        }
+
+        private void OnPauseButtonPressed()
+        {
+            ViewManager.Show<PauseView>();
+            _gameStateMachine.ChangeState(new PauseState(_gameStateMachine));
+        }
 
     }
 }
---
+++ Arcanoid/Assets/Scripts/UI/Views/PauseView.cs
@@ -0,0 +1,82 @@
+using SekiburaGames.Arkanoid.System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace SekiburaGames.Arkanoid.UI
+{
+    public class PauseView : View
+    {
+        [SerializeField]
+        private Slider _sliderMusic;
+        [SerializeField]
+        private Slider _sliderEffects;
+        [SerializeField]
+        private UI_LanguageChooseController _languageChooseController;
+        [SerializeField]
+        private AudioMixerGroup _audioMixer;
+        private bool _isSetSliderValues = false;
+
+        [SerializeField]
+        private Button _helpBtn;
+        [SerializeField]
+        private GameObject _helpView;
+
+        [SerializeField]
+        private Button _mainMenuScene;
+
+        [SerializeField]
+        private Button _resumeBtn;
+
+        private GameStateMachine _gameStateMachine;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            SystemManager.Get(out _gameStateMachine);
+            _sliderEffects.onValueChanged.AddListener((value) => OnSoundEffectsSliderChange(value));
+            _sliderMusic.onValueChanged.AddListener((value) => OnMusicSliderChange(value));
+            _helpBtn.onClick.AddListener(() => _helpView.SetActive(true));
+            _mainMenuScene.onClick.AddListener(() => SceneManager.LoadScene("MainMenuScene"));
+            _resumeBtn.onClick.AddListener(() => OnResumeButtonPressed());
+        }
+
+        public override void Show(object parameter = null)
+        {
+            base.Show(parameter);
+            _languageChooseController.UpdateButtonStates();
+            SlidersUpdateValue();
+        }
+
+        private void SlidersUpdateValue()
+        {
+            _isSetSliderValues = true;
+            //_sliderDialogs.value = saveData.SettingsDialogVolume;
+            //_sliderMusic.value = saveData.SettingsMusicVolume;
+            _isSetSliderValues = false;
+        }
+
+        private void OnSoundEffectsSliderChange(float value)
+        {
+            if (_isSetSliderValues)
+                return;
+            _audioMixer.audioMixer.SetFloat("Effects", Mathf.Log10(value) * 20);
+        }
+
+        private void OnMusicSliderChange(float value)
+        {
+            if (_isSetSliderValues)
+                return;
+            _audioMixer.audioMixer.SetFloat("BackgroundMusic", Mathf.Log10(value) * 20);
+        }
+
+        private void OnResumeButtonPressed()
+        {
+            ViewManager.Show<GameplayView>();
+            _gameStateMachine.ChangeState(new GameplayState(_gameStateMachine));
+        }
+    }
+}
